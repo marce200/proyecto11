69778482e7906742b76cdbd088c43358c9206637
ROOT <ROOT@WIN7ENT30>
2021-01-08 10:39:56 +0100

    Primer cambio en Pincipal. Momento 2

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -3,6 +3,9 @@ package prueba1;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo java-git");
+		System.out.println("Hola Mundo java-git");
+		System.out.println("Hola Mundo java-git");
+		System.out.println("Pruebas Git. Se añade Cambio1");
 	}
 
 }
